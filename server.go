7620b71641216ef3b5c8b6cefe182184246ada17
Naveen Palaniswamy <8531695+palanisn@users.noreply.github.com>
2018-11-12 10:33:39 +1100

    Externalize config params to command line

--- server.go
+++ server.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	. "github.com/palanisn/articles-api/app"
+	"log"
 )
 
 func main() {
 	api := &App{}
-	api.InitializeApp("tester", "p@ssword12#", "articles_store")
-	api.Start(":8080")
+
+	dbUserName := flag.String("dbUserName", "test", "DB user name")
+	dbUserPwd := flag.String("dbUserPwd", "test", "DB user pwd")
+	dbName := flag.String("dbName", "articles_store", "Name of DB schema")
+	port := flag.Int("port", 8080, "Server listening on port")
+
+	flag.Parse()
+
+	api.InitializeApp(*dbUserName, *dbUserPwd, *dbName)
+
+	addrsString := fmt.Sprintf(":%d", *port)
+
+	log.Println(fmt.Sprintf("Server is up and running, listening to requests on port %d ..", *port))
+
+	api.Start(addrsString)
 }
